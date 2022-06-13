030981f99ad0942182e91447e304d9df8cb7031f
ChandTheMan <coreyschmierer97@gmail.com>
2022-06-13 11:25:28 +1000

    Initialising repository

--- lib/ws-lambda-deploy-stack.ts
+++ lib/ws-lambda-deploy-stack.ts
@@ -1,11 +1,22 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { lambda_layer_awscli, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as cdk from 'aws-cdk-lib'
+import * as path from 'path'
 
 export class WsLambdaDeployStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const lambdaFunction = new lambda.Function(this, "practice-lambda", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'practice-lambda.lambda_handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '/../src')),
+      environment: {
+        ACCOUNT_ID: cdk.Stack.of(this).account
+      }
+    })
     // The code that defines your stack goes here
 
     // example resource
@@ -13,4 +24,5 @@ export class WsLambdaDeployStack extends Stack {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
   }
+
 }
